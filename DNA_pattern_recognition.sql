b7a8e70e9f1d7c20b739570af349ac48bb1aa7a7
rutvik <rutvikpatil3896@gmail.com>
2026-03-04 22:45:35 +0530

    added new file

---
+++ DNA_pattern_recognition.sql
@@ -0,0 +1,33 @@
+SELECT 
+    sample_id,
+    dna_sequence,
+    species,
+    -- Starts with ATG
+    CASE 
+        WHEN dna_sequence LIKE 'ATG%' THEN 1 
+        ELSE 0 
+    END AS has_start,
+    
+    -- Ends with TAA, TAG, or TGA
+    CASE 
+        WHEN dna_sequence LIKE '%TAA' 
+          OR dna_sequence LIKE '%TAG' 
+          OR dna_sequence LIKE '%TGA' THEN 1 
+        ELSE 0 
+    END AS has_stop,
+    
+    -- Contains the ATAT motif
+    CASE 
+        WHEN dna_sequence LIKE '%ATAT%' THEN 1 
+        ELSE 0 
+    END AS has_atat,
+    
+    -- Contains at least 3 consecutive G's
+    CASE 
+        WHEN dna_sequence LIKE '%GGG%' THEN 1 
+        ELSE 0 
+    END AS has_ggg
+FROM 
+    Samples
+ORDER BY 
+    sample_id ASC;
